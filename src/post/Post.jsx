c58b25a0c067ecf4f40d164bd32b14d284538d48
amalary95 <Malary.Music@gmail.com>
2021-12-01 15:15:04 -0800

    Posts are where they should be but side bar needs work. Also header fixed so it doesnt cross through the nav bar

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -22,13 +22,14 @@ export default function Post() {
                 </span>
                 <hr/>
                 <span className="postDate">1 day ago...</span>
-
-
             </div>
+            <p className="postDesc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse maxime sit numquam deserunt deleniti, officia corrupti rem ut! Reprehenderit accusantium, recusandae debitis explicabo soluta magnam cupiditate iusto nam dicta quia.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus facere rerum, laboriosam sit similique temporibus veritatis voluptates rem perferendis odio iure adipisci voluptate suscipit, atque dolor. Iste beatae aliquid esse!
+            </p>
+
             
-            
-            
-            
+
         </div>
     )
 }
