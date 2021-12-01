9f31b031842bede4b0d90a6be2ea257797530b61
amalary95 <Malary.Music@gmail.com>
2021-12-01 14:00:35 -0800

    Posts are starting to look ok right now

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -8,6 +8,23 @@ export default function Post() {
             src = "https://images.unsplash.com/photo-1579992193972-f9b55a80aa3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80" alt = " "
             
             />
+            <div className="postInfo">
+
+                <div className="postCats">
+                    
+                    <span className = "postCat">Music</span>
+                    <span className = "postCat">Reviews</span>
+                    <span className = "postCat">Albums</span>
+                    <span className = "postCat">Misc</span>
+                </div>
+                <span className = "postTitle">
+                    Lorem is in the house 
+                </span>
+                <hr/>
+                <span className="postDate">1 day ago...</span>
+
+
+            </div>
             
             
             
