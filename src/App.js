3d7ee8165605eaf00d57842e2a6af06f499bc994
amalary95 <Malary.Music@gmail.com>
2021-12-02 12:31:30 -0800

    Just finished routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
+import React from "react";
 import NavBar from "./Components/navbar/NavBar";
+import Home from "./Components/pages/home/Home"
 import Register from "./Components/pages/register/Register"
+import Login from "./Components/pages/login/Login";
+import Write from "./Components/pages/home/write/Write"
+import Settings from "./Components/pages/home/settings/Settings"
+import Single from "./single/Single"
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+
 
 
 
 
 
 function App() {
+  const user = false;
   return (
     <div> 
+    <Router>
+        <NavBar/>
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+        <Route path="/register">
+            {user ? <Home/>: <Register />}
+        </Route>
+        <Route path="/login">
+            {user ? <Home/>:<Login />}
+        </Route>
+        <Route path="/write">
+            {user ? <Write/>: <Register/>}
+        </Route>
+        <Route path="/settings">
+            {user ? <Settings/>: <Register/>}
+        </Route>
+        <Route path="/post/:postid">
+            <Single />
+        </Route>
 
-      <NavBar/>
-      <Register/>
+
+        </Switch>
+    
+
+
+    </Router>
+
+      
       
       
     
